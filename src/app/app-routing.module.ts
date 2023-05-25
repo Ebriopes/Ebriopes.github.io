ec4b0fbb746844e159434c4ad2169535314b4f6b
ebriopes <antonio.vargasrosales@gmail.com>
2023-05-24 23:09:46 -0600

    :sparkles: Feat: new view landing with routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () =>
+      import('./core/landing/landing.module').then((m) => m.LandingModule),
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      anchorScrolling: 'enabled',
+      scrollPositionRestoration: 'enabled',
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
