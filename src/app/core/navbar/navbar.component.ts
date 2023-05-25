602d5d4f43cfe080f7824a2ce0e93b7ed5be7c86
ebriopes <antonio.vargasrosales@gmail.com>
2023-05-24 23:26:31 -0600

    :sparkles: Feat: update navbar to set tittle, new links & logo

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  @Input() title: string = '';
+  titleArr: string[] = [];
   openNav = false;
+
+  ngOnInit(): void {
+    this.titleArr = this.title.split('');
+  }
 }
