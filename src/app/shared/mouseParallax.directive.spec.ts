b618d0afd7a30b2091150464f14036e0745883b1
ebriopes <antonio.vargasrosales@gmail.com>
2023-05-24 23:32:31 -0600

    :dizzy: Feat: directive to parallax effect with mouse movement

---
+++ src/app/shared/mouseParallax.directive.spec.ts
@@ -0,0 +1,8 @@
+import { MouseParallaxDirective } from './mouseParallax.directive';
+
+describe('ParallaxDirective', () => {
+  it('should create an instance', () => {
+    const directive = new MouseParallaxDirective();
+    expect(directive).toBeTruthy();
+  });
+});
